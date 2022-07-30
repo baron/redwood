ff2cbe56d970b336501bcbe164fcbb87f9745f70
Martin Terneborg <martinterneborg@protonmail.com>
2022-07-30 22:45:13 +0200

    Specify tmux config file

--- src/tmux.rs
+++ src/tmux.rs
@@ -1,12 +1,16 @@
-use tmux_interface::{AttachSession, KillSession, NewSession, SwitchClient};
+use tmux_interface::{AttachSession, KillSession, SwitchClient, TmuxCommand};
 
 use std::env;
+use std::path;
 
 use crate::error::RedwoodError;
 use crate::Result;
 
 pub fn new_session(session_name: &str, start_directory: &str) -> Result<()> {
-    if let Err(e) = NewSession::new()
+    let tmux_config_path = get_tmux_config_path()?;
+    if let Err(e) = TmuxCommand::new()
+        .file(tmux_config_path.to_string_lossy())
+        .new_session()
         .detached()
         .session_name(session_name)
         .start_directory(start_directory)
@@ -52,4 +56,15 @@ pub fn kill_session(session_name: &str) -> Result<()> {
 
 fn in_tmux_session() -> bool {
     env::var_os("TMUX").is_some()
+}
+
+fn get_tmux_config_path() -> Result<path::PathBuf> {
+    let configs_dir_path = if let Some(path) = env::var_os("XDG_CONFIG_HOME") {
+        path::PathBuf::from(path)
+    } else if let Some(path) = env::var_os("HOME") {
+        path::PathBuf::from(path).join(".config")
+    } else {
+        return Err(RedwoodError::ConfigPathUnresolvable);
+    };
+    return Ok(configs_dir_path.join("tmux").join(".tmux.conf"));
 }
